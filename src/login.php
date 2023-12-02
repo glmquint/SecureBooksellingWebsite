a9749ff72fe625fd6da7e271bb3bf1241091fa5c
Guillaume Quint <g.quint@studenti.unipi.it>
2023-12-02 02:28:45 +0100

    naive registration and password hash+salting

--- src/login.php
+++ src/login.php
@@ -3,6 +3,7 @@ session_start();
 // verifyLogin() is a function that verifies the user's login credentials with the database
 function verifyLogin($username, $password): bool
 {
+    /*
     $db = mysqli_connect('localhost', 'root', 'rootroot', 'securebooksellingdb');
     // user prepared statements to prevent SQL injection
     $stmt = mysqli_prepare($db, "SELECT * FROM users WHERE username=? AND password=?");
@@ -15,6 +16,46 @@ function verifyLogin($username, $password): bool
     } else {
         return false;
     }
+    */
+
+
+
+// Retrieve the hashed password from the database based on the username
+// Replace the following lines with your database connection and query
+    $servername = "localhost";
+    $dbusername = "root";
+    $dbpassword = "rootroot";
+    $dbname = "securebooksellingdb";
+
+    $conn = new mysqli($servername, $dbusername, $dbpassword, $dbname);
+
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+
+    $stmt = $conn->prepare("SELECT password FROM users WHERE username=? ");
+    $stmt->bind_param("s", $username);
+    mysqli_stmt_execute($stmt);
+    $result = mysqli_stmt_get_result($stmt);
+    // check if insertion was successful
+    if ($stmt->affected_rows > 0) {
+        // Password hash found in the database
+        $row = mysqli_fetch_array($result);
+        $conn->close();
+        $stored_hashed_password = $row["password"];
+
+        // Verify the entered password against the stored hash
+        if (password_verify($password, $stored_hashed_password)) {
+            return true;
+        } else {
+            return false;
+        }
+    } else {
+        $conn->close();
+        return false;
+    }
+
 }
 
 if (isset($_POST['username']) || isset($_POST['password'])) {
---
+++ src/register.php
@@ -0,0 +1,57 @@
+<?php
+
+if (isset($_POST['username']) || isset($_POST['password'])) {
+
+    // User's inputted password
+    $user_input_password = $_POST['password'];
+
+    // Hash the password using bcrypt
+    $hashed_password = password_hash($user_input_password, PASSWORD_BCRYPT);
+
+    // Store the hashed password in the database
+    $servername = "localhost";
+    $username = "root";
+    $password = "rootroot";
+    $dbname = "securebooksellingdb";
+
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    // use prepared statements to insert into users
+
+    $stmt = $conn->prepare("INSERT INTO users (username, password) VALUES (?, ?)");
+    $stmt->bind_param("ss", $_POST['username'], $hashed_password);
+    $stmt->execute();
+    // check if insertion was successful
+    if ($stmt->affected_rows > 0) {
+        echo "New record created successfully";
+    } else {
+        echo "Error: " . $conn->error;
+    }
+    $conn->close();
+}
+
+?>
+
+<DOCTYPE html>
+    <html lang="en">
+    <head>
+        <title>Secure Book selling website</title>
+        <link rel="stylesheet" href="https://cdn.simplecss.org/simple.min.css">
+    </head>
+    <body>
+    <h1>Register</h1>
+    <form method="post" action="register.php">
+        <label for="username">Username</label>
+        <input type="text" name="username" id="username" required="required">
+        <label for="password">Password</label>
+        <input type="password" name="password" id="password" required="required">
+        <button type="submit">Register</button>
+    </form>
+    </body>
+</html>
+
+    
