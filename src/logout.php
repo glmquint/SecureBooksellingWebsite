625e343b5bd183b7eb3bf46e2e9836c847d5293c
Guillaume Quint <glmquint@gmail.com>
2024-01-05 16:52:34 +0100

    version caddy file with working custom 404 page

--- src/logout.php
+++ src/logout.php
@@ -5,7 +5,7 @@ require_once 'utils/Logger.php';
     session_start_or_expire();
     performLog("Info", "User logged out", array("email" => $_SESSION['email']));
     // reset username
-    $_SESSION['email'] = null;
+    unset($_SESSION['email']);
     // remove remember me cookie
     if (isset($_COOKIE['rememberme'])) {
         unset($_COOKIE['rememberme']);
