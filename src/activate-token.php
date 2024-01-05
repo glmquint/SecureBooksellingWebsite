e077430b830ef150f2f64a18faaf154643ba1370
Guillaume Quint <glmquint@gmail.com>
2024-01-05 20:01:03 +0100

    upgrade activate-token.php to use binary token

--- src/activate-token.php
+++ src/activate-token.php
@@ -2,27 +2,27 @@
 require_once 'utils/dbUtils.php';
 session_start();
 if (isset($_GET['token'])) {
-    $token = $_GET['token'];
+    $token = hex2bin($_GET['token']);
     $userid = getUidFromToken($token);
     if($userid){
         if(deleteToken($token)) {
             if(activateAccount($userid)){
                 $_SESSION['success'] = "Your account was successfully activated";
                 $_SESSION['userid'] = $userid;
-                performLog("Info", "Account activated", array("userid" => $userid, "token" => $token));
+                performLog("Info", "Account activated", array("userid" => $userid, "token" => $_GET['token']));
             }
             else{
-                performLog("Error", "Failed to activate account", array("userid" => $userid, "token" => $token));
+                performLog("Error", "Failed to activate account", array("userid" => $userid, "token" => $_GET['token']));
                 $_SESSION['errorMsg'] = "Something went wrong with your request!";
             }
         }
         else{
-            performLog("Error", "Failed to delete activation token", array("userid" => $userid, "token" => $token));
+            performLog("Error", "Failed to delete activation token", array("userid" => $userid, "token" => $_GET['token']));
             $_SESSION['errorMsg'] = "Something went wrong with your request!";
         }
     }
     else{
-        performLog("Error", "missing user id in activate token", array("userid" => $userid, "token" => $token));
+        performLog("Error", "missing user id in activate token", array("userid" => $userid, "token" => $_GET['token']));
         $_SESSION['errorMsg'] = "Something went wrong with your request!";
     }
 }
