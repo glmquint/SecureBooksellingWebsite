b1aef64c13975980b7963779dad5585b0d625b8b
Fabio <fabizen99@gmail.com>
2024-01-07 10:31:03 +0100

    made comment somewhat usefull

--- src/utils/dbUtils.php
+++ src/utils/dbUtils.php
@@ -5,7 +5,7 @@ $dotenv->load();
 function session_start_or_expire() : void
 {
     session_start();
-    // Expire the session if it hasn't been accessed for more than 30 minutes.
+    // Expire the session if it hasn't been accessed for more than x minutes (set in .env file).
     $maxlifetime =  $_ENV['SESSION_MAX_LIFETIME'];
     if (isset($_SESSION['last_access']) && ((time() - $_SESSION['last_access']) > $maxlifetime)) {
         session_unset();
@@ -32,6 +32,7 @@ class DBConnection {
         }
     }
 
+    // close the database connection when the object is destroyed
     function __destruct()
     {
         if ($this->conn){
@@ -53,18 +54,23 @@ class DBConnection {
 
 function registerUser($mail, $user_input_password): array
 {
+    // Check if the email and password are strings for type safety
     if (!is_string($mail) || !is_string($user_input_password)) {
         performLog("Error", "Invalid type of email, not a string", array("mail" => $mail));
         return [];
     }
+    // Check if the user already exists in the database
     $userArray = getUser($mail);
     if(count($userArray) > 0){
         return array("id" => $userArray['id'], "active" => $userArray['active'], "exists" => true);
     }
+
+    // If not, register the user
     $db = new DBConnection();
     if(!$db->conn){
         die("Connection failed: " . $db->conn->connect_error);
     }
+    // hash the password
     $hashed_password = password_hash($user_input_password, PASSWORD_BCRYPT);
     $stmt = $db->conn->prepare("INSERT INTO users (email, password) VALUES (?, ?)");
     $stmt->bind_param("ss",$mail, $hashed_password);
@@ -81,13 +87,13 @@ function registerUser($mail, $user_input_password): array
 
 function verifyLogin($email, $password): int
 {
+    // Check if the email and password are strings for type safety
     if(!is_string($email) || !is_string($password)) {
         performLog("Error", "Invalid type of email or password, not a string", array("mail" => $email));
         return 0;
     }
-
+    // Check if the login credentials are correct, if the account is active and if the user has not failed to login too many times
     $db = new DBConnection();
-
     $db->stmt = $db->conn->prepare("SELECT password, active FROM users WHERE email=? 
         AND (failed_login_attempts < 3 OR failed_login_time < DATE_SUB(NOW(), INTERVAL 1 MINUTE))");
     $db->stmt->bind_param("s", $email);
@@ -106,6 +112,7 @@ function verifyLogin($email, $password): int
             mysqli_stmt_execute($db->stmt);
             return 1 + $active; // 1 - registered but not yet activated, 2 - registered and mail activated
         } else {
+            // Wrong password, update the failed login attempts
             $db->stmt = $db->conn->prepare("UPDATE users SET failed_login_attempts=failed_login_attempts+1, 
                                                             failed_login_time=NOW() WHERE email=?");
             $db->stmt->bind_param("s", $email);
@@ -120,6 +127,7 @@ function verifyLogin($email, $password): int
 
 function changePassword($email, $newPassword): bool
 {
+    // Check if the email and password are strings for type safety
     if(!is_string($email) || !is_string($newPassword)) {
         performLog("Error", "Invalid type of email or password, not a string", array("mail" => $email));
         return false;
@@ -127,8 +135,6 @@ function changePassword($email, $newPassword): bool
     // Store the hashed password in the database
     $hashed_password = password_hash($newPassword, PASSWORD_BCRYPT);
     $db = new DBConnection();
-
-    // use prepared statements to change the password of a user
     $db->stmt = $db->conn->prepare("UPDATE users SET password=? WHERE email=?");
     $db->stmt->bind_param("ss", $hashed_password, $email);
     $db->stmt->execute();
@@ -138,6 +144,7 @@ function changePassword($email, $newPassword): bool
 
 function changePasswordById($userId, $newPassword): bool
 {
+    // Check if the password are strings for type safety
     if (!is_string($newPassword)) {
         performLog("Error", "Invalid type of password (not a string)", array("id" => $userId));
         return false;
@@ -153,21 +160,22 @@ function changePasswordById($userId, $newPassword): bool
     return ($db->stmt->affected_rows > 0);
 }
 
-// create a function to check if a user exists in the database
 function getUser($email): array
 {
+    // Check if the email is a string for type safety
     if (!is_string($email)) {
         performLog("Error", "Invalid type email, not a string", array("mail" => $email));
         return [];
     }
+    // Get the user from the database
     $db = new DBConnection();
-
     $db->stmt = $db->conn->prepare("SELECT * FROM users WHERE email=? ");
     $db->stmt->bind_param("s", $email);
     mysqli_stmt_execute($db->stmt);
     $result = mysqli_stmt_get_result($db->stmt);
     // check if insertion was successful
     if ($db->stmt->affected_rows > 0) {
+        // Return the id and the account status
         $row = mysqli_fetch_array($result);
         return array("id" => $row["id"], "active" => $row["active"]);
     } else {
@@ -188,74 +196,68 @@ function countToken($userId): int
 
 function saveToken($token, $userId, $time): bool
 {
+    // Check if the there are too many token for a user
     if(countToken($userId)>=3){
         performLog("Warning", "Too many token request for a user", array("id" => $userId));
         return false;
     }
-
+    // Generate a time to live for the token express in datetime
     date_default_timezone_set('Europe/Rome');
-    //generate a time to live for the token express in datetime
     $ttl = date('Y-m-d H:i:s', strtotime('+'. $time .' minutes'));
 
+    // Store the token in the database
     $db = new DBConnection();
-
-    //create a prepared statemt to insert the token, the user and ttl into the database
     $db->stmt = $db->conn->prepare("INSERT INTO reset_token (token, user_id, expiration_date) VALUES (?, ?, ?)");
-    //bind the parameters where token is an integer, username is a strings and ttl is datetime
     $db->stmt->bind_param("sss", $token, $userId, $ttl);
     mysqli_stmt_execute($db->stmt);
     $result = mysqli_stmt_get_result($db->stmt);
-    // check if insertion was successful
+    // Check if insertion was successful
     return ($db->stmt->affected_rows > 0);
 }
 
-//create a function to check if a token exists in the database
 function getUidFromToken($token): int
 {
+    // Check if the token is a string for type safety and if it is false (wrong conversion)
     if(!$token || !is_string($token)) {
         performLog("Error", "Invalid type of token", array("token" => $token));
         return 0;
     }
+    // Get the current date and time
     date_default_timezone_set('Europe/Rome');
-    //get the current date and time
     $currentDate = date('Y-m-d H:i:s');
 
+    // Get the token from the database
     $db = new DBConnection();
-
-    //create a prepare statement to get the token and the expiration_date
     $db->stmt = $db->conn->prepare("SELECT token, expiration_date, user_id FROM reset_token WHERE token=?");
-    //bind the token parameter
     $db->stmt->bind_param("s", $token);
     mysqli_stmt_execute($db->stmt);
     $result = mysqli_stmt_get_result($db->stmt);
-    // check if insertion was successful
+    // Check if insertion was successful
     if ($db->stmt->affected_rows > 0) {
         $row = mysqli_fetch_array($result);
-        //check if the token is expired
+        // Check if the token is expired
         if ($row["expiration_date"] > $currentDate) {
             return $row["user_id"];
         } else {
-            // token exists but is expired
+            // Token exists but is expired
             return 0;
         }
     } else {
-        // token does not exist
+        // Token does not exist
         return 0;
     }
 
 }
 
-// delete a token from the database
 function deleteToken($token): bool
 {
+    // Check if the token is a string for type safety and if it is false (wrong conversion)
     if(!$token || !is_string($token)) {
         performLog("Error", "Invalid type of token", array("token" => $token));
         return false;
     }
+    // Delete the token from the database and the expired ones
     $db = new DBConnection();
-
-    //create a prepare statement to delete the current token from the database
-    // also remove all expired tokens to keep the database clean
     $db->stmt = $db->conn->prepare("DELETE FROM reset_token WHERE token=? OR expiration_date < NOW()");
     //bind the token parameter
     $db->stmt->bind_param("s", $token);
@@ -268,8 +270,6 @@ function deleteToken($token): bool
 function activateAccount($userId): bool
 {
     $db = new DBConnection();
-
-    //create a prepare statement to delete the token from the database
     $stmt = $db->conn->prepare("UPDATE users SET active=1 WHERE id=?");
     //bind the token parameter
     $stmt->bind_param("i", $userId);
@@ -281,10 +281,12 @@ function activateAccount($userId): bool
 
 function getUserID($email): int
 {
+    // Check if the email is a string for type safety
     if(!is_string($email)) {
         performLog("Error", "Invalid type of email, not a string", array("mail" => $email));
         return 0;
     }
+    // Get the user id from the database
     $db = new DBConnection();
     $db->stmt = $db->conn->prepare("SELECT id FROM users WHERE email=?");
 
