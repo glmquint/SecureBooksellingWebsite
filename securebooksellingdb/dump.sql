3dfb4af5feea2f2c90d5bbb0cb941877eb43e0f7
Guillaume Quint <glmquint@gmail.com>
2024-01-09 09:44:06 +0100

    export db to include cover_path in books table

--- securebooksellingdb/dump.sql
+++ securebooksellingdb/dump.sql
@@ -1,8 +1,8 @@
 CREATE DATABASE  IF NOT EXISTS `securebooksellingdb` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `securebooksellingdb`;
--- MySQL dump 10.13  Distrib 8.0.34, for macos13 (arm64)
+-- MySQL dump 10.13  Distrib 8.0.18, for Win64 (x86_64)
 --
--- Host: localhost    Database: securebooksellingdb
+-- Host: 127.0.0.1    Database: securebooksellingdb
 -- ------------------------------------------------------
 -- Server version	8.2.0
 
@@ -31,6 +31,7 @@ CREATE TABLE `books` (
   `author` varchar(45) DEFAULT 'Unknwon',
   `available` int DEFAULT '0',
   `synopsis` text,
+  `cover_path` varchar(128) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_UNIQUE` (`id`),
   UNIQUE KEY `title_UNIQUE` (`title`),
@@ -44,7 +45,7 @@ CREATE TABLE `books` (
 
 LOCK TABLES `books` WRITE;
 /*!40000 ALTER TABLE `books` DISABLE KEYS */;
-INSERT INTO `books` VALUES (1,'A Tale of Two Cities',1099,'Charles Dickens',100,'A Tale of Two Cities is a historical novel published in 1859 by Charles Dickens, set in London and Paris before and during the French Revolution. The novel tells the story of the French Doctor Manette, his 18-year-long imprisonment in the Bastille in Paris, and his release to live in London with his daughter Lucie whom he had never met. The story is set against the conditions that led up to the French Revolution and the Reign of Terror.'),(2,'The Little Prince',836,'Antoine de Saint-Exup√©rie',100,'The Little Prince (French: Le Petit Prince, pronounced [l…ô p(…ô)ti p Å…õÃÉs]) is a novella written and illustrated by French aristocrat, writer, and military pilot Antoine de Saint-Exup√©ry. It was first published in English and French in the United States by Reynal & Hitchcock in April 1943 and was published posthumously in France following liberation; Saint-Exup√©ry\'s works had been banned by the Vichy Regime. The story follows a young prince who visits various planets, including Earth, and addresses themes of loneliness, friendship, love, and loss. Despite its style as a children\'s book, The Little Prince makes observations about life, adults, and human nature.'),(3,'Harry Potter and the Philosopher\'s Stone',1200,'J.K. Rowling',100,'Harry Potter and the Philosopher\'s Stone is a fantasy novel written by British author J. K. Rowling. The first novel in the Harry Potter series and Rowling\'s debut novel, it follows Harry Potter, a young wizard who discovers his magical heritage on his eleventh birthday, when he receives a letter of acceptance to Hogwarts School of Witchcraft and Wizardry. Harry makes close friends and a few enemies during his first year at the school and with the help of his friends, Ron Weasley and Hermione Granger, he faces an attempted comeback by the dark wizard Lord Voldemort, who killed Harry\'s parents, but failed to kill Harry when he was just 15 months old.'),(4,'And Then There Were None',1150,'Agatha Christie',100,'And Then There Were None is a mystery novel by the English writer Agatha Christie, who described it as the most difficult of her books to write.[2] It was first published in the United Kingdom by the Collins Crime Club on 6 November 1939, as Ten Little Niggers,[3] after an 1869 minstrel song that serves as a major plot element.[4][5] The US edition was released in January 1940 with the title And Then There Were None, taken from the last five words of the song.[6] Successive American reprints and adaptations use that title, though American Pocket Books paperbacks used the title Ten Little Indians between 1964 and 1986. UK editions continued to use the original title until 1985'),(5,'Dream of the Red Chamber',1510,'Cao Xueqin',100,'Dream of the Red Chamber (Honglou Meng) or The Story of the Stone (Shitou Ji) is a Chinese novel composed by Cao Xueqin in the mid-18th century. One of the Four Great Classical Novels of Chinese literature, it is known for its psychological scope, and its observation of the worldview, aesthetics, lifestyles, and social relations of 18th-century China'),(6,'The Hobbit',990,'J.R.R. Tolkien',100,'The Hobbit, or There and Back Again is a children\'s fantasy novel by English author J. R. R. Tolkien. It was published in 1937 to wide critical acclaim, being nominated for the Carnegie Medal and awarded a prize from the New York Herald Tribune for best juvenile fiction. The book is recognized as a classic in children\'s literature, and is one of the best-selling books of all time with over 100 million copies sold.'),(7,'The Alchemist',1000,'Paulo Coelho',2,'The Alchemist (Portuguese: O Alquimista) is a novel by Brazilian author Paulo Coelho which was first published in 1988. Originally written in Portuguese, it became a widely translated international bestseller. The story follows the shepherd boy Santiago in his journey across northern Africa to the pyramids of Egypt after he dreams of finding a treasure there');
+INSERT INTO `books` VALUES (1,'A Tale of Two Cities',1099,'Charles Dickens',100,'A Tale of Two Cities is a historical novel published in 1859 by Charles Dickens, set in London and Paris before and during the French Revolution. The novel tells the story of the French Doctor Manette, his 18-year-long imprisonment in the Bastille in Paris, and his release to live in London with his daughter Lucie whom he had never met. The story is set against the conditions that led up to the French Revolution and the Reign of Terror.','images/A Tale of Two Cities-cover.png'),(2,'The Little Prince',836,'Antoine de Saint-Exup√©rie',100,'The Little Prince (French: Le Petit Prince, pronounced [l…ô p(…ô)ti p Å…õÃÉs]) is a novella written and illustrated by French aristocrat, writer, and military pilot Antoine de Saint-Exup√©ry. It was first published in English and French in the United States by Reynal & Hitchcock in April 1943 and was published posthumously in France following liberation; Saint-Exup√©ry\'s works had been banned by the Vichy Regime. The story follows a young prince who visits various planets, including Earth, and addresses themes of loneliness, friendship, love, and loss. Despite its style as a children\'s book, The Little Prince makes observations about life, adults, and human nature.','images/The Little Prince-cover.png'),(3,'Harry Potter and the Philosopher\'s Stone',1200,'J.K. Rowling',99,'Harry Potter and the Philosopher\'s Stone is a fantasy novel written by British author J. K. Rowling. The first novel in the Harry Potter series and Rowling\'s debut novel, it follows Harry Potter, a young wizard who discovers his magical heritage on his eleventh birthday, when he receives a letter of acceptance to Hogwarts School of Witchcraft and Wizardry. Harry makes close friends and a few enemies during his first year at the school and with the help of his friends, Ron Weasley and Hermione Granger, he faces an attempted comeback by the dark wizard Lord Voldemort, who killed Harry\'s parents, but failed to kill Harry when he was just 15 months old.','images/Harry Potter and the Philosopher\'s Stone-cover.png'),(4,'And Then There Were None',1150,'Agatha Christie',98,'And Then There Were None is a mystery novel by the English writer Agatha Christie, who described it as the most difficult of her books to write.[2] It was first published in the United Kingdom by the Collins Crime Club on 6 November 1939, as Ten Little Niggers,[3] after an 1869 minstrel song that serves as a major plot element.[4][5] The US edition was released in January 1940 with the title And Then There Were None, taken from the last five words of the song.[6] Successive American reprints and adaptations use that title, though American Pocket Books paperbacks used the title Ten Little Indians between 1964 and 1986. UK editions continued to use the original title until 1985','images/And Then There Were None-cover.png'),(5,'Dream of the Red Chamber',1510,'Cao Xueqin',100,'Dream of the Red Chamber (Honglou Meng) or The Story of the Stone (Shitou Ji) is a Chinese novel composed by Cao Xueqin in the mid-18th century. One of the Four Great Classical Novels of Chinese literature, it is known for its psychological scope, and its observation of the worldview, aesthetics, lifestyles, and social relations of 18th-century China','images/Dream of the Red Chamber-cover.png'),(6,'The Hobbit',990,'J.R.R. Tolkien',100,'The Hobbit, or There and Back Again is a children\'s fantasy novel by English author J. R. R. Tolkien. It was published in 1937 to wide critical acclaim, being nominated for the Carnegie Medal and awarded a prize from the New York Herald Tribune for best juvenile fiction. The book is recognized as a classic in children\'s literature, and is one of the best-selling books of all time with over 100 million copies sold.','images/The Hobbit-cover.png'),(7,'The Alchemist',1000,'Paulo Coelho',2,'The Alchemist (Portuguese: O Alquimista) is a novel by Brazilian author Paulo Coelho which was first published in 1988. Originally written in Portuguese, it became a widely translated international bestseller. The story follows the shepherd boy Santiago in his journey across northern Africa to the pyramids of Egypt after he dreams of finding a treasure there','images/The Alchemist-cover.png');
 /*!40000 ALTER TABLE `books` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -71,7 +72,7 @@ CREATE TABLE `carts` (
 
 LOCK TABLES `carts` WRITE;
 /*!40000 ALTER TABLE `carts` DISABLE KEYS */;
-INSERT INTO `carts` VALUES (234255,1,3),(250033,3,1),(250033,4,1);
+INSERT INTO `carts` VALUES (234255,1,3),(250033,3,1),(250033,4,1),(260870,3,1),(398642,7,4),(525373,4,2);
 /*!40000 ALTER TABLE `carts` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -102,7 +103,7 @@ CREATE TABLE `orders` (
 
 LOCK TABLES `orders` WRITE;
 /*!40000 ALTER TABLE `orders` DISABLE KEYS */;
-INSERT INTO `orders` VALUES ('473357',6,234255,'a',3297,'in transit'),('936090',6,250033,'a',2350,'in transit');
+INSERT INTO `orders` VALUES ('193356',4,260870,'asdf',1200,'in transit'),('496178',4,398642,'asdf',4000,'waiting for restock'),('119633',4,525373,'asdf',2300,'in transit'),('473357',6,234255,'a',3297,'in transit'),('936090',6,250033,'a',2350,'in transit');
 /*!40000 ALTER TABLE `orders` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -129,7 +130,7 @@ CREATE TABLE `purchases` (
 
 LOCK TABLES `purchases` WRITE;
 /*!40000 ALTER TABLE `purchases` DISABLE KEYS */;
-INSERT INTO `purchases` VALUES (6,1),(6,3),(6,4);
+INSERT INTO `purchases` VALUES (6,1),(4,3),(6,3),(4,4),(6,4),(4,7);
 /*!40000 ALTER TABLE `purchases` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -156,6 +157,7 @@ CREATE TABLE `reset_token` (
 
 LOCK TABLES `reset_token` WRITE;
 /*!40000 ALTER TABLE `reset_token` DISABLE KEYS */;
+INSERT INTO `reset_token` VALUES (_binary 'BdG\‘&\ÏmÄ•˘n¢ìñ',16,'2024-01-08 12:47:10'),(_binary 'meß{Åô™M:vµÇn\⁄C	',19,'2024-01-08 12:51:28'),(_binary '\ﬂ41≥¢ˇŸóTÄ\‚GÉçê',17,'2024-01-08 12:49:30'),(_binary '\‚TXh™dttòtÿÖ$æU\Ë',4,'2024-01-08 11:58:13'),(_binary '\ÎC∑ól∞únÑz≤ñD?i\…',18,'2024-01-08 12:50:18');
 /*!40000 ALTER TABLE `reset_token` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -176,7 +178,7 @@ CREATE TABLE `users` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_UNIQUE` (`id`),
   UNIQUE KEY `email_UNIQUE` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -185,7 +187,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (4,'ghi0m@localhost.com','$2y$10$u9REmg63ZzMbtCclEe7FUO.hUAVtxSL1eC4RmAOLvDPMXAavtnLha',0,'2023-12-16 10:50:54',1),(5,'fabi0@localhost.com','$2y$10$MdaisEyyuy6TwmSSo8ipDOue6T/dmrDgdHzoQjvkm75eXDFFkeXCu',0,'2023-12-18 16:59:58',1),(6,'giacom0@localhost.com','$2y$10$BUUPclzGBPrJSKILGyKh7.Z0ULvlcrM6MRrdIoDAxjfIDVnZZNuXm',0,'1970-01-01 00:00:00',1);
+INSERT INTO `users` VALUES (4,'ghi0m@localhost.com','$2y$10$3BhgHBnlHGN7vMhgBEfwXuQjX8wqo7s478ZpMd0ZC7Oq4TuW4YqIW',0,'2024-01-08 11:18:46',1),(5,'fabi0@localhost.com','$2y$10$MdaisEyyuy6TwmSSo8ipDOue6T/dmrDgdHzoQjvkm75eXDFFkeXCu',0,'2023-12-18 16:59:58',1),(6,'giacom0@localhost.com','$2y$10$BUUPclzGBPrJSKILGyKh7.Z0ULvlcrM6MRrdIoDAxjfIDVnZZNuXm',0,'1970-01-01 00:00:00',1),(16,'glmquint@gmail.com','$2y$10$AqGnFOquYL.pH7poV5pV/e29/dA038dae3noIRwDeL92hMDpeL0A2',0,'1970-01-01 00:00:00',0),(17,'hide@localhost.com','$2y$10$mRNzab4nNZNmqQFN8rtwa.4hCLOVSfGTq8tHRxzrRtqoZL9cjwANe',0,'1970-01-01 00:00:00',0),(18,'asdfadfa@23452345','$2y$10$mmO1lVTgdMKwFJv/ai56LelszYYyeMmX2dehllyKZcwZOWG46GPwi',0,'1970-01-01 00:00:00',0),(19,'hide2@gmail.com','$2y$10$tcArnNn5pcWwhR2x1tRGguzqUDbClNEojGzh5vcQ8ziN5XI0OACP6',0,'1970-01-01 00:00:00',0);
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -198,4 +200,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-01-07 17:17:49
+-- Dump completed on 2024-01-09  9:42:01
